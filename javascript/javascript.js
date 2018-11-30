9fa960053ffb7ce2bdc860ec5f52fdc86251bd4c
Madalena Sousa <34659824+MadalenaSousa@users.noreply.github.com>
2018-11-30 18:54:13 +0000

    Dropdown menu done

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,27 +2,7 @@ console.log(document.querySelector(".paginas .col-1:nth-child(2)"));
 
 let botaoPortfolio = document.querySelector(".paginas .col-1:nth-child(2)");
 
+
 botaoPortfolio.addEventListener("click", function () {
-
-    var newItem = document.createElement("div");
-    newItem.innerHTML = "<div class=\"options\">\n" +
-        "                                <div>\n" +
-        "                                    <a href=\"portfolio.html\">\n" +
-        "                                        2018\n" +
-        "                                    </a>\n" +
-        "                                </div>\n" +
-        "                                <div>\n" +
-        "                                    <a href=\"\">\n" +
-        "                                        2017\n" +
-        "                                    </a>\n" +
-        "                                </div>\n" +
-        "                                <div>\n" +
-        "                                    <a href=\"\">\n" +
-        "                                        2016\n" +
-        "                                    </a>\n" +
-        "                                </div>\n" +
-        "                            </div>";
-
-    botaoPortfolio.appendChild(newItem);
+    document.querySelector("nav .list").classList.toggle("show");
 });
-
