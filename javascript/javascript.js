c0b6aae36c11ed25f656ea52445a36aec786768d
Madalena Sousa <34659824+MadalenaSousa@users.noreply.github.com>
2018-11-30 10:45:36 +0000

    scroll menu

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,28 @@
-document.querySelectorAll("nav [class*='col-'].empty img").addEventListener("click", function () {
-    this.style.cssText = "backgroundColor: blue";
+console.log(document.querySelector(".paginas .col-1:nth-child(2)"));
+
+let botaoPortfolio = document.querySelector(".paginas .col-1:nth-child(2)");
+
+botaoPortfolio.addEventListener("click", function () {
+
+    var newItem = document.createElement("div");
+    newItem.innerHTML = "<div class=\"options\">\n" +
+        "                                <div>\n" +
+        "                                    <a href=\"portfolio.html\">\n" +
+        "                                        2018\n" +
+        "                                    </a>\n" +
+        "                                </div>\n" +
+        "                                <div>\n" +
+        "                                    <a href=\"\">\n" +
+        "                                        2017\n" +
+        "                                    </a>\n" +
+        "                                </div>\n" +
+        "                                <div>\n" +
+        "                                    <a href=\"\">\n" +
+        "                                        2016\n" +
+        "                                    </a>\n" +
+        "                                </div>\n" +
+        "                            </div>";
+
+    botaoPortfolio.appendChild(newItem);
 });
+
