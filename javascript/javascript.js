7e00dad960e72641b8d68ede0b39c35902c79705
Madalena Sousa <34659824+MadalenaSousa@users.noreply.github.com>
2018-12-04 18:47:16 +0000

    Updates

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -8,10 +8,24 @@ botaoPortfolio.addEventListener("click", dropdown(opcoesPortfolio));
 let hamburguer = document.querySelector("nav .burguer");
 let opcoes = document.querySelector(".paginas");
 
-console.log(hamburguer);
-console.log(opcoes);
-
 hamburguer.addEventListener("click", dropdown(opcoes));
+
+//loja
+let box = document.querySelectorAll(".box");
+let check = document.querySelectorAll(".check");
+
+for(let i=0; i<check.length; i++) {
+    box[i].addEventListener("click", dropdown(check[i]));
+}
+
+let cor = document.querySelectorAll(".cores img");
+console.log(cor);
+
+for(let i=0; i<cor.length; i++) {
+    cor[i].addEventListener("click", function () {
+        cor[i].classList.toggle("border");
+    });
+}
 
 //Função dropmenu
 function dropdown(menu) {
