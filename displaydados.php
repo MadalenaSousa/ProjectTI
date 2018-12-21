3528500110b4cbe82a8a2c571b50e1b3a3b704b0
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2018-12-21 14:58:39 +0000

    tentativa de acento

--- displaydados.php
+++ displaydados.php
@@ -29,8 +29,6 @@
             <div class="col-8">
                 <?php
 
-                header("Content-Type: text/html; charset=ISO-8859-1", true);
-
                 $filename = "data.txt";
                 $nome = $_POST["name"];
                 $mail = $_POST["email"];
