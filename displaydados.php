9d96d3bfb82ca0d4992b6eb44ec20165cc39a1cc
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2018-12-21 11:41:39 +0000

    mail

--- displaydados.php
+++ displaydados.php
@@ -34,37 +34,67 @@
                 $validade = $_POST["validade"];
                 $codigo	= $_POST["codigo"];
 
-                htmlOutputForm($nome, $mail, $numero, $morada, $cartao);
+                htmlOutputForm($nome, $mail, $numero, $morada, $cartao, $validade, $codigo);
 
-                fileOutputForm($nome, $mail, $numero, $morada, $cartao, $filename);
+                fileOutputForm($nome, $mail, $numero, $morada, $cartao, $validade, $codigo, $filename);
 
-                function htmlOutputForm($name, $email, $tlmvl, $morada, $cartao)
+                function htmlOutputForm($nome, $mail, $numero, $morada, $cartao, $validade, $codigo)
                 {
                     echo '<table>';
 
                     echo '<tr>';
-                    echo '<th>Nome</th><th>Email</th><th>Número de Telemóvel</th><th>Morada</th><th>Número do Cartão</th>';
+                    echo '<th>Nome</th><th>Email</th><th>Telemóvel</th><th>Morada</th><th>Nº de Cartão</th><th>Validade</th><th>Código</th>';
                     echo '</tr>';
 
                     echo '<tr>';
-                    echo "<td>$name</td><td>$email</td><td>$tlmvl</td><td>$morada</td><td>$cartao</td>";
+                    echo "<td>$nome</td><td>$mail</td><td>$numero</td><td>$morada</td><td>$cartao</td><td>$validade</td><td>$codigo</td>";
                     echo '</tr>';
 
                     echo '</table>';
                 }
 
 
-                function fileOutputForm($name, $email, $tlmvl, $morada, $cartao, $filename)
+                function fileOutputForm($nome, $mail, $numero, $morada, $cartao, $validade, $codigo, $filename)
                 {
-                    $line_to_write = $name . ', ';
-                    $line_to_write = $line_to_write . $email . ',';
+                    $line_to_write = $nome . ', ';
+                    $line_to_write = $line_to_write . $mail . ',';
 
-                    $line_to_write = $line_to_write . $tlmvl . ',';
+                    $line_to_write = $line_to_write . $numero . ',';
                     $line_to_write = $line_to_write . $morada;
                     $line_to_write = $line_to_write . $cartao;
+                    $line_to_write = $line_to_write . $validade;
+                    $line_to_write = $line_to_write . $codigo;
                     $line_to_write = $line_to_write . "\n";
 
                     file_put_contents($filename, $line_to_write, FILE_APPEND);
+                }
+
+                mailOutputForm($nome, $mail, $numero, $morada, $cartao, $validade, $codigo);
+
+
+                function mailOutputForm($nome, $mail, $numero, $morada, $cartao, $validade, $codigo)
+                {
+
+                    $message = "Saudacoes,
+Este mail serve de confirmacao de dados. Por favor, não responda a este mail. 
+   
+Dados:
+Nome: $nome
+Email: $mail
+Numero de Telemovel: $numero
+Morada: $morada
+Numero de cartao: $cartao
+Validade: $validade
+Codigo de Seguranca: $codigo
+ 
+Cumprimentos,
+MMDesign
+    ";
+
+                    mail($mail,
+                        'Form submitted',
+                        $message,
+                        'From: mmdesign@example.com');
                 }
                 
                 ?>
