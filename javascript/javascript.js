c30d35f19f29d24cca906cb9d1c6b9b9fd23d1ae
Madalena Sousa <34659824+MadalenaSousa@users.noreply.github.com>
2018-12-22 23:03:14 +0000

    cart dropdown

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -25,3 +25,58 @@ function dropdown(menu) {
     };
 }
 
+let artigo = document.getElementsByClassName("id");
+let quantity = document.getElementsByClassName("quant");
+let lista = document.querySelector("header .list-itens");
+
+artigoDetail(0);
+
+function artigoDetail(index) {
+    if (index >= artigo.length) {
+        return;
+    }
+
+    fetch("https://jsonplaceholder.typicode.com/photos/" + artigo[index].innerText)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            console.log(json);
+
+            lista.appendChild(article(json, quantity[index].innerText));
+
+            artigoDetail(index+1);
+
+        });
+}
+
+function article(json, quant) {
+    let link = document.createElement("a");
+    link.setAttribute("href", "artigo.php?id=" + json.id);
+
+    let row = document.createElement("div");
+    row.classList.add("col-1");
+
+    let imagem = document.createElement("img");
+    imagem.setAttribute("src", json.thumbnailUrl);
+    imagem.setAttribute("alt", json.title);
+    imagem.setAttribute("width", "30px");
+    imagem.classList.add("icon");
+
+    let descricao = document.createElement("span");
+    descricao.innerText = " " + json.id + "€" + " ";
+    descricao.style.fontSize = "10pt";
+
+    let quantidade = document.createElement("span");
+    quantidade.innerText = "x" + quant;
+    quantidade.style.fontSize = "10pt";
+
+    row.appendChild(imagem);
+    row.appendChild(descricao);
+    row.appendChild(quantidade);
+
+    link.appendChild(row);
+
+    return link;
+}
+
